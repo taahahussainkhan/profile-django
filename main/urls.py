2998b90b8a35e6148cf383f60203a1c607bf55fe
taahahussainkhan <taaha128@gmail.com>
2024-01-05 01:00:59 +0500

    setup

---
+++ main/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+app_name = "main"
+
+urlpatterns = [
+    # path('',views.IndexView.as_view(),name="home"),
+    # path('contact/',views.ContactView.as_view(),name="contact"),
+    # path('portfolio/',views.PortfolioDetailView.as_view(),name="portfolio"),
+    # path('blogs/',views.BlogView.as_view(),name="blogs"),
+    # path('blog/<slug:slug>/',views.BlogDetailView.as_view(),name="blog"),
+    
+]
